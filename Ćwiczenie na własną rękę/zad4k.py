8bae9eb5818100de657c88d6d248b55550b61568
DroppedHard <46939111+DroppedHard@users.noreply.github.com>
2022-06-02 16:06:45 +0200

    Update zad4k.py

--- Ćwiczenie na własną rękę/zad4k.py
+++ Ćwiczenie na własną rękę/zad4k.py
@@ -1,5 +1,15 @@
-from zad4ktesty import runtests
-
+# from zad4ktesty import runtests
+"""
+Zadanie 4 - Ścieżka Falisza
+Szablon rozwiązania: zad4k.py
+Dana jest mapa wyrażona poprzez tablicę dwuwymiarową wymiarów N x N zawierająca liczby naturalne. Król Falisz znajduje się na polu (0,0) tej tablicy. Jego celem jest dojście do pola (n-1, n-1) i w trakcie tego procesu oblanie jak najmniejszej liczby studentów (każde pole tablicy wyraża ilość studentów, która zostanie oblana, gdy król przejdzie przez to pole). Ze względu na regulamin studiów Falisz może poruszać się jedynie o 1 pole w prawo lub w dół. Proszę napisać algorytm, który określi jaka jest minimalna liczba studentów, która zostanie oblana aby król doszedł do celu. Dla ułatwienia zadania pola (0, 0) oraz (n-1, n-1) przyjmują stałą wartość 0.
+Algorytm należy zaimplementować jako funkcję postaci:
+def falisz( T ): …
+która przyjmuje dwuwymiarową tablicę liczb naturalnych T i zwraca liczbę będącą minimalną ilością studentów, których król musi oblać.
+Przykład. Dla tablicy:
+T = [ [0, 5, 4, 3], [2, 1, 3, 2], [8, 2, 5, 1], [4, 3, 2, 0] ]
+Wynikiem jest liczba 9
+"""
 
 def uwalanie(tab, n, i, j, cache):
     if i == j == 0:
@@ -27,4 +37,4 @@ def falisz (T):
     return odp
 
 
-runtests ( falisz )
+# runtests ( falisz )
