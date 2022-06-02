83970b66f714c01e634a76e1af979340b24ece42
DroppedHard <46939111+DroppedHard@users.noreply.github.com>
2022-06-02 16:07:47 +0200

    Update zad6k.py

--- Ćwiczenie na własną rękę/zad6k.py
+++ Ćwiczenie na własną rękę/zad6k.py
@@ -1,4 +1,15 @@
-from zad6ktesty import runtests 
+# from zad6ktesty import runtests 
+"""
+Zadanie 6 - Hasło do laptopa
+Szablon rozwiązania: zad6k.py
+Podczas Twoich praktyk zawodowych w biurze śledczym otrzymałeś zadanie dostania się do pewnego zabezpieczonego hasłem laptopa. Jedyną podpowiedzią jaką zostawił przestępca był pewien ciąg cyfr wyrażony jako string S. Odkryto już, że w rzeczywistości podpowiedź pozostawiona przez przestępcę była pewną tajną wiadomością, która została zakodowana poprzez zamienienie liter na znaki (tak np. A = 1, B = 2, …, Z = 26) oraz, że hasło ustawione przez przestępcę to tak naprawdę liczba wyrażająca całkowitą liczbę różnych wiadomości, które mogą ukrywać się pod zakodowanym ciągiem. Twoim zadaniem jest napisanie algorytmu, który zwróci poprawne hasło niezbędne do zalogowania się do laptopa. Możesz przyjąć, że pusty ciąg ma tylko 1 rozwiązanie, a niepoprawne wiadomości 0 rozwiązań (przez niepoprawne można uznać np. takie, które posiadają dwa zera pod rząd, z których nie da się odczytać żadnej litery)
+Algorytm należy zaimplementować jako funkcję postaci:
+def haslo( S ): …
+która przyjmuje string S i zwraca liczbę będącą poprawnym hasłem do laptopa.
+Przykład. Dla ciągu znaków:
+S = "123"
+Wynikiem jest liczba 3 ponieważ zaszyfrowana wiadomość "123" może zostać zakodowane jako "ABC" (123), "LC" (12 3) lub "AW" (1 23).
+"""
 
 
 def haslo (S):
@@ -32,4 +43,4 @@ def haslo (S):
         return cache[n-1]
 
 
-runtests ( haslo )
+# runtests ( haslo )
