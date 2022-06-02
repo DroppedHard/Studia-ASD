ae662767e5b8713b1700b7a43c06fdbb7dcb3d3c
DroppedHard <46939111+DroppedHard@users.noreply.github.com>
2022-06-02 16:06:09 +0200

    Update zad3k.py

--- Ćwiczenie na własną rękę/zad3k.py
+++ Ćwiczenie na własną rękę/zad3k.py
@@ -1,5 +1,15 @@
-from zad3ktesty import runtests
-
+# from zad3ktesty import runtests
+"""
+Zadanie 3 - Najmniejsza k-ładna suma
+Szablon rozwiązania: zad3k.py
+Dla każdego ciągu n liczb możemy obliczyć k-ładną sumę (Zakładamy, że k <= n). Poprzez k-ładną sumę rozumiemy minimalną sumę pewnych liczb wybranych w ten sposób, że z każdych k kolejnych elementów wybraliśmy przynajmniej jeden z nich (w szczególności oznacza to, że dla k=1 musimy wybrać wszystkie elementy, a dla k=n wystarczy wybrać jeden, najmniejszy z nich). Proszę napisać algorytm, który dla zadanej tablicy liczb naturalnych oraz wartości k oblicza k-ładną sumę.
+Algorytm należy zaimplementować jako funkcję postaci:
+def ksuma( T, k ): …
+która przyjmuje tablicę liczb naturalnych T = [a1, a2, …, an] oraz liczbę naturalną k.
+Przykład. Dla tablicy:
+[1, 2, 3, 4, 6, 15, 8, 7] oraz k = 4
+Wynikiem jest liczba 7
+"""
 
 def kladna(tab, i, k, cache):
     if i-k < 0:
@@ -35,4 +45,4 @@ def ksuma( T, k ):
     return ladnasuma
 
 
-runtests ( ksuma )
+# runtests ( ksuma )
